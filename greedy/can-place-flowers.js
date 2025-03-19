6162f4589859e46e80322faae63d9b6ac0763353
Mohamed Yusri <mohamedyusri205@gmail.com>
2025-03-19 17:53:49 +0200

    LC-can-place-flowers [Runtime: 0 ms; Memory: 57.1 MB]

--- greedy/can-place-flowers.js
+++ greedy/can-place-flowers.js
@@ -1,17 +1,18 @@
-/**
- * @param {number[]} flowerbed
- * @param {number} n
- * @return {boolean}
- */
 var canPlaceFlowers = function(flowerbed, n) {
-    // if(flowerbed[0]==flowerbed[1]==1){
-    //     n--;
-    // }
-    for (let i =1 ;i<flowerbed.length-1;i++){
-        if(flowerbed[i-1]==flowerbed[i]==flowerbed[i+1]==0){
+    if (n === 0) return true;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (
+            flowerbed[i] === 0 && 
+            (i === 0 || flowerbed[i - 1] === 0) &
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0) 
+        ) {
+            flowerbed[i] = 1;
             n--;
+
+            if (n === 0) return true;
         }
     }
 
-    return n==0
+    return false;
 };
