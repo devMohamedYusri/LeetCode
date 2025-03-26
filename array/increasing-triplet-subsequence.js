12853102a64b07f420274a07bb85a44c446b28aa
Mohamed Yusri <mohamedyusri205@gmail.com>
2025-03-26 13:05:06 +0200

    LC-increasing-triplet-subsequence [Runtime: 1 ms; Memory: 74.3 MB]

--- array/increasing-triplet-subsequence.js
+++ array/increasing-triplet-subsequence.js
@@ -3,21 +3,16 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-    if(nums.length<3){
-        return false;
-    }
-    let x=nums[0],y=nums[1],z=nums[2];
-    for(let i =1; i<(nums.length-1);i++){
-        if(x<y && y<z){
-            return true;
+    let first=Infinity;
+    let second =Infinity;
+    for(const num of nums){
+        if(first>=num){
+            first=num;
+        }else if (second>=num){
+            second=num;
         }else{
-            x=nums[i-1];
-            y=nums[i];
-            z=nums[i+1];
+            return true;
         }
-         if(x<y && y<z){
-            return true;}
     }
     return false;
-
 };
