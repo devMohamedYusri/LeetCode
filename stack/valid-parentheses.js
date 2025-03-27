52628e729cd0ea3fb52a9067b602f94dc84deaab
Mohamed Yusri <mohamedyusri205@gmail.com>
2025-03-27 13:31:55 +0200

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ stack/valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack=[];
+    const opened=['(','{','['];
+   const bracketPairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    for(const i of s){
+        if(opened.includes(i)){
+            stack.push(i)
+        }else{
+           const top=stack.pop();
+           if(top!==bracketPairs[i]){
+            return false
+           }
+        }
+    }
+
+    return true;
+};
