3bc2c42e79ec293b863c913cb54dc3ba838c2c32
Mohamed Yusri <mohamedyusri205@gmail.com>
2025-03-30 16:27:22 +0200

    LC-remove-duplicates-from-sorted-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    const dummy=new ListNode(head.val);
+    let current=dummy;
+    while(head){
+                    console.log("out",head);
+        if(current.val==head.val){
+            head=head.next;
+               console.log("if",head);
+
+        }else{
+            current.next=head;
+            head=head.next;
+            current=current.next;
+         console.log("else",head);
+        }
+    }
+
+    current.next=null;
+    return dummy;
+};
